e76a4dac70686ee2724218ed802f3870c6eaeac5
c-campos <carlos.campos1@louisiana.edu>
2021-10-26 09:48:54 -0500

    Initial review notes

---
+++ Review.R
@@ -0,0 +1,10 @@
+# Exam 2 Review
+    
+    #Github version control
+    # 1. create a github repository with a readme file
+    # 2. Copy repository link
+    # 3. Create new verion control directory in R and paste github link
+    # 4. Create a new script and save within the corresponding directory that the R script is saved in, the file shoul appear under git tab on right of console
+    # 5. Now any changes you save will be noted on Git version -- git compares the document you saved to what exists in your Github repository.
+    # 6. If you want to save the changes to your file on Github, you need to first COMMIT your files wich will stage them and make them available for review by collaborators (if available) then PUSH to main branch
+    # TIPS: if your work is ever ahead of your repository, 
